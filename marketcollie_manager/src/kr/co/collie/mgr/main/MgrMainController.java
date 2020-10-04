5f3c724e5d4b80f86dc5cf1cac2af2064b880b19
knewbean <knewbean@daum.net>
2020-10-04 20:23:51 +0900

    initialize project

---
+++ marketcollie_manager/src/kr/co/collie/mgr/main/MgrMainController.java
@@ -0,0 +1,15 @@
+package kr.co.collie.mgr.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MgrMainController {
+	
+	@RequestMapping(value="/index.do", method=RequestMethod.GET)
+	public String main() {
+		return "main";
+	}
+
+}
