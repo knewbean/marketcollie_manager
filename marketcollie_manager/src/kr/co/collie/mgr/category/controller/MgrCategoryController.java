73cdfdf6a549d540e25468acd9ffd9ec18a32736
sist24 <sist24@DESKTOP-M2NGUUQ>
2020-10-06 17:23:01 +0900

    header.jsp, MgrMainController, MgrMemberController, MgrCategoryController, view_cate_list.jsp, view_member_slit.jsp 수정

--- marketcollie_manager/src/kr/co/collie/mgr/category/controller/MgrCategoryController.java
+++ marketcollie_manager/src/kr/co/collie/mgr/category/controller/MgrCategoryController.java
@@ -1,5 +1,18 @@
 package kr.co.collie.mgr.category.controller;
 
-public class MgrCategoryController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-}
+@Controller
+public class MgrCategoryController {
+	
+	@RequestMapping(value="/category/list.do", method=GET)
+	public String getCategoryList(Model model) {
+		
+		
+		return "cate/view_cate_list";
+	}//getCategoryList
+	
+}//class
