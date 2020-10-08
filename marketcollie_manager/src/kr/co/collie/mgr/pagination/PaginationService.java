17bd4b62d28a6820abf481935cb118a547c1a8b6
yl <sist24@DESKTOP-M2NGUUQ>
2020-10-08 16:34:57 +0900

    pagination수정

--- marketcollie_manager/src/kr/co/collie/mgr/pagination/PaginationService.java
+++ marketcollie_manager/src/kr/co/collie/mgr/pagination/PaginationService.java
@@ -85,6 +85,35 @@ public class PaginationService {
 		return pVO;
 	}//calcPage
 	
+	public PageVO calcPaging(int current_page, int total_cnt) {
+		PageVO pVO=null;
+		
+		int page_scale=pageScale();
+		int total_page=(int)Math.ceil((double)total_cnt/page_scale);
+		
+		int page_range=5;
+		int start_page=((current_page-1)/page_range)*page_range+1;
+		int end_page=start_page+page_range-1;
+		if(total_page < end_page) {
+			end_page = total_page;
+		}//end if
+		
+		int pre_page=current_page - 1;
+		int next_page=current_page + 1;
+		
+		if( pre_page < 1 ) { //"이전" 버튼을 비활성화하는 조건
+			pre_page=current_page;
+		}//end if
+		
+		if( end_page<next_page ){ //"다음" 버튼을 비활성화하는 조건
+			next_page=current_page;
+		}//end if
+		
+		pVO=new PageVO(current_page, pre_page, next_page, start_page, end_page);
+		
+		return pVO;
+	}//calcPage
+	
 	/**
 	 * 현재 페이지와 테이블명, 컬럼명, 컬럼값을 저장하는 TotalCntVO를 매개변수로 받아 
 	 * 게시글 밑에 추가되는 페이지네이션 view 코드를 String으로 리턴해주는 method
@@ -97,6 +126,71 @@ public class PaginationService {
 	public String getPagination(int currentPage, TotalCntVO tcVO) {
 		StringBuilder view =new StringBuilder();
 		PageVO pVO=calcPaging(currentPage, tcVO);
+		
+		view.append("<nav aria-label='Page navigation example'>\n")
+		.append("<ul class='pagination justify-content-center'>\n")
+		.append("<li class='page-item");
+		if(pVO.getPre_page() < 1) {
+			view.append(" disabled'>\n");
+		}else {
+			view.append(" active'>\n");
+		}//end else
+		view.append("<a class='page-link'");
+		if( pVO.getPre_page() >= 1) {
+			view.append(" onclick='movePage(")
+			.append(pVO.getPre_page())
+			.append(")'"); 
+		}//end if
+		view.append(" aria-label='Previous'>\n")
+		.append("<span aria-hidden='true'>&laquo;</span>\n")
+		.append("</a>\n")
+		.append("</li>\n");
+		
+		for(int i=pVO.getStart_page(); i<=pVO.getEnd_page(); i++) {
+			view.append("<li class=\"page-item\">")
+			.append("<a class='page-link'");
+			if( pVO.getCurrent_page() != i) {
+				view.append(" onclick='movePage(")
+				.append(i)
+				.append(")'"); 
+			}//end if
+			view.append(">\n")
+			.append(i)
+			.append("</a>\n")
+			.append("</li>\n");
+		}//end for
+		
+		view.append("<li class='page-item");
+		if(pVO.getEnd_page() < pVO.getNext_page()) {
+			view.append(" disabled'>\n");
+		}else {
+			view.append(" active'>\n");
+		}//end else
+		view.append("<a class='page-link'");
+		if( pVO.getEnd_page() >= pVO.getNext_page()) {
+			view.append(" onclick='movePage(")
+			.append(pVO.getNext_page())
+			.append(")'"); 
+		}//end if
+		view.append(" aria-label='Next'>\n")
+		.append("<span aria-hidden='true'>&raquo;</span>\n")
+		.append("</a>\n")
+		.append("</li>\n")
+		.append("</ul>\n")
+		.append("</nav>\n");
+		
+		return view.toString();
+	}//getPagination
+	
+	/**
+	 * 테이블을 join해야하는 경우 사용할 method
+	 * @param currentPage
+	 * @param total_cnt
+	 * @return
+	 */
+	public String getPagination(int currentPage, int total_cnt) {
+		StringBuilder view =new StringBuilder();
+		PageVO pVO=calcPaging(currentPage, total_cnt);
 		
 		view.append("<nav aria-label='Page navigation example'>\n")
 		.append("<ul class='pagination justify-content-center'>\n")
